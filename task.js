3acb95bb0de09921f47e1853330d3c07dc9d9916
Natalija-Virt <virtnatalie@gmail.com>
2018-01-02 00:36:21 +0200

    added the second parameter

--- task.js
+++ task.js
@@ -1,17 +1,27 @@
-function getTotalPrice1(str) {
+function getTotalPrice1(str, regExpr) {
     if(typeof(str) === 'string') {
-        str = str.replace(',', '');
-        str = str.match('[0-9]+\.[0-9]{2}');
+        const thousandSeparator = ',';
+        str = str.replace(thousandSeparator, '');
+        str = str.match(regExpr);
         return 'Total price is: ' +str[0] + '$';
     }
     else return NaN;
 }
-function getTotalPrice2(str) {
+function getTotalPrice2(str, regExpr) {
     if(typeof(str) === 'string') {
-        return 'Total price is: ' +str.replace(/[^0-9.]+/g, '') + '$';
+        return 'Total price is: ' +str.replace(regExpr, '') + '$';
+    }
+    else return NaN;
+}
+function getTotalPrice3(str, regExpr) {
+    if(typeof(str) === 'string') {
+        const thousandSeparator = ',';
+        str = str.replace(thousandSeparator, '');
+        return 'Total price is: ' +regExpr.exec(str)[0] + '$';
     }
     else return NaN;
 }
 let price = document.querySelector("#lineTotal_3").innerText;
-console.log('First function: ' + getTotalPrice1(price));
-console.log('Second function: ' + getTotalPrice2(price));
+console.log('First function: ' + getTotalPrice1(price, '[0-9]+\.[0-9]{2}'));
+console.log('Second function: ' + getTotalPrice2(price, /[^0-9.]+/g));
+console.log('Third function: ' + getTotalPrice3(price, new RegExp('[0-9]+\.[0-9]{2}','')));
