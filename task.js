7d0ffb036823e299f54f594c2cb788aa3c1c4c61
Natalia <virtnatalie@gmail.com>
2018-01-04 18:06:32 +0200

    Corrected again

--- task.js
+++ task.js
@@ -5,8 +5,8 @@ const listOfElements = [...document.querySelectorAll('.codes-wrapper ul li span'
 
 function getTotalPrice(str, codesObj) {
     const originPrice = document.querySelector('.total-wrapper h1 span:nth-child(2)').innerText.replace(/\$/, '');
-    let code = str.match(/'(.*)'/)[1];
-    let price = parseFloat(str.match(/[0-9]+\.[0-9]{2}/g));
+    let code = str.match(/'(.*[^'])'/)[1];
+    let price = parseFloat(str.match(/[0-9]+\.[0-9]{2}/));
     let discount = originPrice - price;
     codesObj[code] = discount;
     return codesObj
@@ -36,9 +36,10 @@ function getBestCodeAccordingToDiscount(discountsObj, bestDiscount) {
             arrayOfBestDiscount.push(key);
         }
     }
-    return bestCodeObj = {
+    bestCodeObj = {
         bestCode: arrayOfBestDiscount
     }
+    return bestCodeObj;
 }
 
 function getOriginalPrice() {
