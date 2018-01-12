564527e48f140a9735390b0a92466ce008060cd0
Natalia <virtnatalie@gmail.com>
2018-01-12 12:08:05 +0200

    Added removedCode function

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -53,3 +53,57 @@ removeCode("NEW2GLHT")
   response => console.log(response),
   error => console.log(error)
 );
+
+function anotherApplyCode(code) {
+
+  return new Promise((resolve, reject) => {
+
+	  const newReq = new XMLHttpRequest();
+    const formData = new FormData();
+    const url = `https://www.hottopic.com/on/demandware.store/Sites-hottopic-Site/default/Cart-AddCouponJson?couponCode=${code}&format=ajax`;
+    formData.append('couponCode', code);
+    formData.append('format', 'ajax');
+    newReq.open('GET', url, true);
+    newReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    newReq.onload = function() {
+      resolve(this.response);
+    };
+    newReq.onerror = function() {
+      reject(console.log(error));
+    };
+    newReq.send(formData);
+  });
+}
+
+anotherApplyCode("NEW2GLHT")
+.then(
+  response => console.log(response),
+  error => console.log(error)
+);
+
+function anotherRemoveCode(code) {
+
+  return new Promise((resolve, reject) => {
+
+	const newReq = new XMLHttpRequest();
+    const formData = new FormData();
+    const url = `https://www.hottopic.com/on/demandware.store/Sites-hottopic-Site/default/COBilling-RemoveCouponJson?couponCode=+${code}&format=ajax`;
+    formData.append('couponCode', code);
+    formData.append('format', 'ajax');
+    newReq.open('GET', url, true);
+    newReq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    newReq.onload = function() {
+      resolve(this.response);
+    };
+    newReq.onerror = function() {
+      reject(console.log(error));
+    };
+    newReq.send(formData);
+  });
+}
+
+anotherRemoveCode("NEW2GLHT")
+.then(
+  response => console.log(response),
+  error => console.log(error)
+);
